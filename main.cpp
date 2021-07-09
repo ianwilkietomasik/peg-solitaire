b517b134628446aa4a3f8520587aa5ace7cfaccd
Ian Wilkie Tomasik <ianwtmjj98@gmail.com>
2021-07-09 15:25:39 -0400

    added call to solver in main method

--- main.cpp
+++ main.cpp
@@ -1,5 +1,11 @@
-#include "ps_state.hpp"
+#include "peg_solitaire.hpp"
 #include <iostream>
 int main(){
+  PegSolitaireSolver solver;
+  PegSolitaireState state;
+  std::vector<Move> soln = solver.solve(state);
+  for(auto m : soln){
+    m.print();
+  }
   return 0;
 }
